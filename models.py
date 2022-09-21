1d30bf01ca6a5e546fc567a3cb3460172e68b0f8
ArnabPushilal <68045951+ArnabPushilal@users.noreply.github.com>
2022-09-21 22:13:59 +0100

    Update models.py

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-
+# BASELINE MF MODEL WITH AND WITHOUT BIAS
 
 class MF(nn.Module):
 
@@ -61,6 +61,8 @@ class MF(nn.Module):
         
         
         return score
+    
+#CONTENT BOOSTED MF
 
 class MF_ing(nn.Module):
 
@@ -114,65 +116,12 @@ class MF_ing(nn.Module):
         
         
         return score
-
-class NeuralCF(nn.Module):
-
-    def __init__(self,number_users,number_hstate,number_recipes,number_ingredients):
     
 
+
+
+#BASELINE NCF
     
-        super(NeuralCF,self).__init__()
-        self.user_embedding = nn.Embedding(number_users+1, number_hstate, padding_idx=0)
-        #self.recipes_embedding = nn.Embedding(number_recipes+1, number_hstate, padding_idx=0)
-        self.ingredients_embedding=nn.Embedding(number_ingredients+1,number_hstate, padding_idx=0)
-        
-
-#        self.conv1=nn.Conv1d()
-
-        self.lin1=nn.Linear(number_hstate,100)
-        self.lin2=nn.Linear(100,50)
-        self.lin3=nn.Linear(50,1)
-        self.dropout_1=nn.Dropout(p=0.3)
-
-        # self.lin21=nn.Linear(number_hstate,100)
-        # self.lin22=nn.Linear(100,50)
-
-        self.lin31=nn.Linear(number_hstate,100)
-        self.lin32=nn.Linear(100,50)
-        self.dropout_2=nn.Dropout(p=0.3)
-        self.lin33=nn.Linear(50,1)
-
-    def forward(self,x):
-        
-
-        ing_emb=self.ingredients_embedding(x['ingredients'])
-
-        recipe_emb=torch.sum(ing_emb,axis=1)
-
-        r1=self.lin1(recipe_emb)
-  
-        r1=F.relu(r1)
-        r1=self.lin2(r1)
-        r1=self.dropout_1(r1)
-        r1=F.relu(r1)
-        r1=self.lin3(r1)
-      
-
-        user_emb=self.user_embedding(x['user'])
-        r2=self.lin31(user_emb)
-        r2=F.relu(r2)
-        r2=self.dropout_2(r2)
-        r2=self.lin32(r2)
-        r2=F.relu(r2)
-        r2=self.lin33(r2)
-
-
-
-        score = (r2 * r1) #.sum(1)
-        #print(score.shape,"shape")
-        return torch.squeeze(score)
-
-
 class NeuralCF_og(nn.Module):
 
     def __init__(self,number_users,number_hstate,number_recipes,number_ingredients):
@@ -233,7 +182,7 @@ class NeuralCF_og(nn.Module):
      
         return torch.squeeze(score)
 
-
+#TWO TOWER MODEL
 
 class NeuralCF_2(nn.Module):
 
@@ -293,6 +242,8 @@ class NeuralCF_2(nn.Module):
 
      
         return torch.squeeze(score)
+    
+#OUR MODEL
 
 class NeuralCF_h(nn.Module):
 
@@ -407,217 +358,7 @@ class NeuralCF_h(nn.Module):
         #print(score.shape,"shape")
         return torch.squeeze(score),health
 
-class NeuralCF_new_self(nn.Module):
-
-    def __init__(self,number_users,number_hstate,number_recipes,number_ingredients):
-    
-
-    
-        super(NeuralCF_new_self,self).__init__()
-        self.user_embedding = nn.Embedding(number_users+1, number_hstate, padding_idx=0)
-        self.recipes_embedding = nn.Embedding(number_recipes+1, number_hstate, padding_idx=0)
-        self.ingredients_embedding=nn.Embedding(number_ingredients+1,number_hstate, padding_idx=0)
-        self.pad_idx=0
-        
-
-#        self.conv1=nn.Conv1d()
-        # trans_i = nn.TransformerEncoderLayer(d_model=number_hstate, nhead=8,batch_first=True)
-        # self.transformer_encoder = nn.TransformerEncoder(trans_i, num_layers=4)
-
-
-        self.att_i=nn.MultiheadAttention(number_hstate,8,batch_first=True,dropout=0)
-        self.att_u=nn.MultiheadAttention(number_hstate,8,batch_first=True,dropout=0)
-        
-        #self.attention=Attention(50)
-
-
-        self.lin1=nn.Linear(number_hstate,100)
-        self.lin2=nn.Linear(100,50)
-        self.lin3=nn.Linear(50,10)
-        self.health=nn.Linear(50,7)
-        self.dropout_1=nn.Dropout(p=0.3)
-
-
-        self.final=nn.Linear(30,10)
-        self.final_2=nn.Linear(10,1)
-
-        # self.lin21=nn.Linear(number_hstate,100)
-        # self.lin22=nn.Linear(100,50)
-
-        self.lin31=nn.Linear(number_hstate,100)
-        self.lin32=nn.Linear(100,50)
-        self.dropout_2=nn.Dropout(p=0.3)
-        self.lin33=nn.Linear(50,10)
-
-        self.lin41=nn.Linear(number_hstate*2,100)
-        self.lin42=nn.Linear(100,50)
-        self.dropout_3=nn.Dropout(p=0.3)
-        self.lin43=nn.Linear(50,10)
-
-    def forward(self,x):
-        
-        #user,recipe,ing,health=x
-
-        padding_mask=(x['ingredients']==self.pad_idx)
-    
-        ing_emb=self.ingredients_embedding(x['ingredients'])
-        ing_emb_feat,weights=self.att_i(ing_emb,ing_emb,ing_emb,key_padding_mask=padding_mask)
-
-        #ing_emb_feat=ing_emb+ing_emb
-
-        #ing_emb=self.transformer_encoder(ing_emb,src_key_padding_mask=padding_mask)
-        user_emb=self.user_embedding(x['user'])
-        user_emb_1=user_emb[:,None,:]
-
-        #print(user_emb.shape)
-        recipe_emb=self.recipes_embedding(x['recipe'])
-        # recipe_emb=recipe_emb[:,None,:]
-        feat_concat_2=torch.concat((recipe_emb,user_emb),dim=1)
-
-        r=self.lin41(feat_concat_2)
-        r=F.relu(r)
-        r=self.lin42(r)
-        r=F.relu(r)
-        r=self.dropout_3(r)
-        r=self.lin43(r)
-
-        recipe,weights=self.att_u(user_emb_1,ing_emb_feat,ing_emb_feat,key_padding_mask=padding_mask)
-        recipe=torch.squeeze(recipe)
-
-        #feat_concat=torch.concat((recipe,user_emb),dim=1)
-
-      
-        r1=self.lin1(recipe)
-        r1=F.relu(r1)
-        r1=self.lin2(r1)
-        r1=self.dropout_1(r1)
-        r1=F.relu(r1)
-
-        health=self.health(r1)
-        r1=self.lin3(r1)
-
-        r2=self.lin31(user_emb)
-        r2=F.relu(r2)
-        r2=self.dropout_2(r2)
-        r2=self.lin32(r2)
-        r2=F.relu(r2)
-        r2=self.lin33(r2)
-
-        final_cat=torch.concat((r,r1,r2),dim=1)
-        final=F.relu(self.final(final_cat))
-        final=self.final_2(final)
-
-        
-        
-        #score = (r1*r2) #.sum(1)
-        #print(score.shape,"shape")
-        return torch.squeeze(final),health
-
-
-class NeuralCF_pre_trained(nn.Module):
-
-    def __init__(self,number_users,number_hstate,number_recipes,number_ingredients):
-    
-
-    
-        super(NeuralCF_pre_trained,self).__init__()
-        self.user_embedding = nn.Embedding(number_users+1, number_hstate, padding_idx=0)
-        #self.recipes_embedding = nn.Embedding(number_recipes+1, number_hstate, padding_idx=0)
-        self.ingredients_embedding=nn.Embedding(number_ingredients+1,number_hstate, padding_idx=0)
-        self.pad_idx=0
-        
-
-#        self.conv1=nn.Conv1d()
-        #trans_i = nn.TransformerEncoderLayer(d_model=number_hstate, nhead=8,batch_first=True)
-        #self.transformer_encoder = nn.TransformerEncoder(trans_i, num_layers=4)
-
-
-        self.att_i=nn.MultiheadAttention(number_hstate,8,batch_first=True,dropout=0.1)
-        self.att_u=nn.MultiheadAttention(number_hstate,8,batch_first=True,dropout=0.1)
-        
-        #self.attention=Attention(50)
-
-
-        self.lin1=nn.Linear(number_hstate,100)
-        self.lin2=nn.Linear(100,50)
-        self.lin3=nn.Linear(50,1)
-        self.health=nn.Linear(50,7)
-        self.dropout_1=nn.Dropout(p=0.3)
-
-
-        # self.final=nn.Linear(100,10)
-        # self.final_2=nn.Linear(10,1)
-
-        # self.lin21=nn.Linear(number_hstate,100)
-        # self.lin22=nn.Linear(100,50)
-
-        self.lin31=nn.Linear(number_hstate,100)
-        self.lin32=nn.Linear(100,50)
-        self.dropout_2=nn.Dropout(p=0.3)
-        self.lin33=nn.Linear(50,1)
-        
-    def from_pretrained(self):
-
-     embeddings=torch.FloatTensor(np.load('ing_emb.npy'))
-     self.ingredients_embedding.weight = torch.nn.Parameter(embeddings)
-     #self.ingredients_embedding.weight.requires_grad =False
-     
-
-    def forward(self,x):
-        
-        #user,recipe,ing,health=x
-
-        padding_mask=(x['ingredients']==self.pad_idx)
-    
-        ing_emb=self.ingredients_embedding(x['ingredients'])
-        
-        ing_emb_feat,weights=self.att_i(ing_emb,ing_emb,ing_emb,key_padding_mask=padding_mask)
-
-        #ing_emb_feat=ing_emb+ing_emb
-
-        #ing_emb=self.transformer_encoder(ing_emb,src_key_padding_mask=padding_mask)
-        user_emb=self.user_embedding(x['user'])
-        user_emb_1=user_emb[:,None,:]
-
-        #print(user_emb.shape)
-        # recipe_emb=self.recipes_embedding(x['recipe'])
-        # recipe_emb=recipe_emb[:,None,:]
-         
-        recipe,weights=self.att_u(user_emb_1,ing_emb_feat,ing_emb_feat,key_padding_mask=padding_mask)
-        recipe=torch.squeeze(recipe)
-        #print(recipe.shape,"shape")
-  
-        #recipe,weights=self.att_r(recipe_emb,ing_emb,ing_emb)
-        #recipe=torch.squeeze(recipe)
-        #recipe_emb=torch.sum(ing_emb,axis=1)
-        #recipe_emb=torch.squeeze(torch.permute(i,(0,2,1))@weights)
-        #print(recipe_emb.shape)
-
-        r1=self.lin1(recipe)
-        r1=F.relu(r1)
-        r1=self.lin2(r1)
-        r1=self.dropout_1(r1)
-        r1=F.relu(r1)
-
-        health=self.health(r1)
-
-        r1=self.lin3(r1)
-        
-        r2=self.lin31(user_emb)
-        r2=F.relu(r2)
-        r2=self.dropout_2(r2)
-        r2=self.lin32(r2)
-        r2=F.relu(r2)
-        r2=self.lin33(r2)
-
-        #final=torch.concat((r1,r2),dim=1)
-
-        #final=F.relu(self.final(final))
-        #final=self.final_2(final)
-
-        score = (r2 * r1) #.sum(1)
-        #print(score.shape,"shape")
-        return torch.squeeze(score)#,health
+#AutoInt without recipe
 
 class NeuralCF_low_without_recipe(nn.Module):
 
@@ -694,6 +435,9 @@ class NeuralCF_low_without_recipe(nn.Module):
     
         return torch.squeeze(r1)#,health
 
+    
+#AutoInt without recipe ( 2 layers of self-attention)
+
 class NeuralCF_low_without_recipe_feat_combo(nn.Module):
 
     def __init__(self,number_users,number_hstate,number_recipes,number_ingredients):
@@ -766,7 +510,7 @@ class NeuralCF_low_without_recipe_feat_combo(nn.Module):
         r1=self.lin3(r1)
         return torch.squeeze(r1)#,health
 
-
+#(Auto Int with recipe embedding)
 
 class NeuralCF_low(nn.Module):
 
@@ -844,77 +588,6 @@ class NeuralCF_low(nn.Module):
         return torch.squeeze(r1)#,health
 
 
-
-
-
-
-        
-class NeuralCF_conv(nn.Module):
-
-    def __init__(self,number_users,number_hstate,number_recipes,number_ingredients):
-    
-
-    
-        super(NeuralCF_conv,self).__init__()
-        self.user_embedding = nn.Embedding(number_users+1, number_hstate, padding_idx=0)
-        #self.recipes_embedding = nn.Embedding(number_recipes+1, number_hstate, padding_idx=0)
-        self.ingredients_embedding=nn.Embedding(number_ingredients+1,number_hstate, padding_idx=0)
-
-        self.conv=nn.Conv1d(43,12,3,stride=1,padding='same')
-        self.conv_1=nn.Conv1d(12,1,3,stride=1,padding='same')
-        
-
-        #self.conv1=nn.Conv1d()
-
-        self.lin1=nn.Linear(number_hstate,100)
-        self.lin2=nn.Linear(100,50)
-        self.lin3=nn.Linear(50,1)
-        self.dropout_1=nn.Dropout(p=0.3)
-
-        # self.lin21=nn.Linear(number_hstate,100)
-        # self.lin22=nn.Linear(100,50)
-
-        self.lin31=nn.Linear(number_hstate,100)
-        self.lin32=nn.Linear(100,50)
-        self.dropout_2=nn.Dropout(p=0.3)
-        self.lin33=nn.Linear(50,1)
-
-    def forward(self,x):
-        
-
-        ing_emb=self.ingredients_embedding(x['ingredients'])
-
-        ing_emb=F.relu(self.conv(ing_emb))
-        recipe_emb=F.relu(self.conv_1(ing_emb))
-        recipe_emb=torch.squeeze(recipe_emb)
-
-      
-        r1=self.lin1(recipe_emb)
-  
-        r1=F.relu(r1)
-        r1=self.lin2(r1)
-        r1=self.dropout_1(r1)
-        r1=F.relu(r1)
-        r1=self.lin3(r1)
-      
-
-        user_emb=self.user_embedding(x['user'])
-        r2=self.lin31(user_emb)
-        r2=F.relu(r2)
-        r2=self.dropout_2(r2)
-        r2=self.lin32(r2)
-        r2=F.relu(r2)
-        r2=self.lin33(r2)
-
-
-
-        score = (r2 * r1) #.sum(1)
-        #print(score.shape,"shape")
-        return torch.squeeze(score)
-
-
-
-
 class NeuralCF_h(nn.Module):
 
     def __init__(self,number_users,number_hstate,number_recipes,number_ingredients):
@@ -927,11 +600,6 @@ class NeuralCF_h(nn.Module):
         self.ingredients_embedding=nn.Embedding(number_ingredients+1,number_hstate, padding_idx=0)
         self.pad_idx=0
         
-
-#        self.conv1=nn.Conv1d()
-        # trans_i = nn.TransformerEncoderLayer(d_model=number_hstate, nhead=8,batch_first=True)
-        # self.transformer_encoder = nn.TransformerEncoder(trans_i, num_layers=4)
-
 
         self.att_i=nn.MultiheadAttention(number_hstate,8,batch_first=True,dropout=0)
         self.att_u=nn.MultiheadAttention(number_hstate,8,batch_first=True,dropout=0)
@@ -1027,6 +695,8 @@ class NeuralCF_h(nn.Module):
         score = (r2 *r1) #.sum(1)
         #print(score.shape,"shape")
         return torch.squeeze(score)#,health
+    
+#Our model with recipe embeddings(ABLATION)
 
 class NeuralCF_new_self_with_recipe_att(nn.Module):
 
@@ -1130,90 +800,5 @@ class NeuralCF_new_self_with_recipe_att(nn.Module):
         #score = (r1*r2) #.sum(1)
         #print(score.shape,"shape")
         return torch.squeeze(final)
-
-
-
-class NeuralCF_h_tags(nn.Module):
-
-    def __init__(self,number_users,number_hstate,number_recipes,number_ingredients,number_tags):
-    
-
-    
-        super(NeuralCF_h_tags,self).__init__()
-        self.user_embedding = nn.Embedding(number_users+1, number_hstate, padding_idx=0)
-        self.recipes_embedding = nn.Embedding(number_recipes+1, number_hstate, padding_idx=0)
-        self.tag_embedding= nn.Embedding(number_tags+1, number_hstate, padding_idx=0)
-        self.ingredients_embedding=nn.Embedding(number_ingredients+1,number_hstate, padding_idx=0)
-        self.pad_idx=0
-        
-
-#        self.conv1=nn.Conv1d()
-        # trans_i = nn.TransformerEncoderLayer(d_model=number_hstate, nhead=8,batch_first=True)
-        # self.transformer_encoder = nn.TransformerEncoder(trans_i, num_layers=4)
-
-
-        self.att_i=nn.MultiheadAttention(number_hstate,8,batch_first=True,dropout=0.1)
-        self.att_u=nn.MultiheadAttention(number_hstate,8,batch_first=True,dropout=0.1)
-        self.att_t=nn.MultiheadAttention(number_hstate,8,batch_first=True,dropout=0.1)
-        
-     
-        self.lin1=nn.Linear(2*number_hstate,100)
-        self.lin2=nn.Linear(100,50)
-        self.lin3=nn.Linear(50,1)
-        self.dropout_1=nn.Dropout(p=0.3)
-
-    
-
-        self.lin31=nn.Linear(number_hstate,100)
-        self.lin32=nn.Linear(100,50)
-        self.dropout_2=nn.Dropout(p=0.3)
-        self.lin33=nn.Linear(50,1)
-
-
-
-    def forward(self,x):
-        
-        #user,recipe,ing,health=x
-
-        padding_mask=(x['ingredients']==self.pad_idx)
-        padding_mask_tags=(x['tags']==self.pad_idx)
-    
-        ing_emb=self.ingredients_embedding(x['ingredients'])
-     
-        ing_emb_feat,weights=self.att_i(ing_emb,ing_emb,ing_emb,key_padding_mask=padding_mask)
-
-        user_emb=self.user_embedding(x['user'])
-        user_emb_1=user_emb[:,None,:]
-
-
-        tag_emb=self.tag_embedding(x['tags'])
-
-
-        recipe,weights=self.att_u(user_emb_1,ing_emb_feat,ing_emb_feat,key_padding_mask=padding_mask)
-        recipe=torch.squeeze(recipe)
-
-        tags,weights=self.att_t(user_emb_1,tag_emb,tag_emb,key_padding_mask=padding_mask_tags)
-        tags=torch.squeeze(tags)
-
-        feat_concat=torch.concat((tags,recipe),dim=1)
-      
-        r1=self.lin1(feat_concat)
-        r1=F.relu(r1)
-        r1=self.lin2(r1)
-        r1=self.dropout_1(r1)
-        r1=F.relu(r1)
-        r1=self.lin3(r1)
-        
-        r2=self.lin31(user_emb)
-        r2=F.relu(r2)
-        r2=self.dropout_2(r2)
-        r2=self.lin32(r2)
-        r2=F.relu(r2)
-        r2=self.lin33(r2)
-
-        score = (r2 *r1)
-       
-        return torch.squeeze(score)#,health
-
 
 
